48d1a1a9f9d4fdb7772759eaf984522162abb76b
Bop4yH <aleksandr.kuzmin2000@gmail.com>
2020-09-22 03:42:38 +0700

    lab2

---
+++ OSI_labs_5sem/lab2/lab2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define SUCCESS 0
+
+void pthread_error(const char *const msg, int error) {
+    fprintf(stderr, "%s: %s", msg, strerror(error));
+    exit(1);
+}
+
+void print_str(char *str) {
+    for (int i = 0; i < 10; i++) {
+        printf("%s\n", str);
+    }
+}
+
+void *child_body(void* arg) {
+    print_str("child");
+
+    return NULL;
+}
+
+int main() {
+    pthread_t child;
+    int error = pthread_create(&child, NULL, child_body, NULL);
+    if (error != SUCCESS) {
+        pthread_error("pthread_create() failed", error);
+    }
+
+    error = pthread_join(child, NULL);
+    if (error != SUCCESS) {
+        pthread_error("pthread_join() failed", error);
+    }
+
+    print_str("parent");
+
+    pthread_exit(NULL);
+}
