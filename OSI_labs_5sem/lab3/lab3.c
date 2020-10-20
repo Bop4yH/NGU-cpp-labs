64d315b144c05bd6cac55239407d3bfbb4551ff5
Aleksandr Kuzmin <a.kuzmin1@g.nsu.ru>
2020-10-20 16:11:51 +0700

    patch 1.0

--- OSI_labs_5sem/lab3/lab3.c
+++ OSI_labs_5sem/lab3/lab3.c
@@ -7,38 +7,43 @@
 #define THREADS 4
 #define BUF_SIZE 256
 
-void pthread_error(const char *const msg, int error) {
-    fprintf(stderr, "%s: %s", msg, strerror(error));
+void pthread_error(const char *const msg, int error, int id) {
+    fprintf(stderr, "%s: %d %s", msg, id, strerror(error));
     exit(1);
 }
 
 void *child_body(void* arg) {
-    for (char **line = arg; *line != NULL; line++) {
-        printf("%s\n", *line);
-    }
+    unsigned long int id = pthread_self();
+    char** lines = (char**)arg;
 
+    for (int i = 0; i < BUF_SIZE; i++){
+        if(lines[i] != NULL) {
+            printf("child %lu %s\n", id, lines[i]);
+        }
+    }
     return NULL;
 }
 
 int main() {
-    char *child_p[THREADS][BUF_SIZE] = {{"child 1 line 1", "child 1 line 2", "child 1 line 3", NULL},
-                                        {"child 2 line 1", "child 2 line 2", "child 2 line 3", NULL},
-                                        {"child 3 line 1", "child 3 line 2", "child 3 line 3", NULL},
-                                        {"child 4 line 1", "child 4 line 2", "child 4 line 3", NULL}};
+    char *child_p[THREADS][BUF_SIZE] = {{"line 1", "line 2", "line 3", NULL},
+                                        {"line 4", "line 5", NULL},
+                                        {"line 6", NULL},
+                                        {"line 7", "line 8", "line 9", NULL}};
 
     pthread_t child[THREADS];
     int error;
     for (int i = 0; i < THREADS; i++) {
+
         error = pthread_create(&child[i], NULL, child_body, child_p[i]);
         if (error != 0) {
-            pthread_error("pthread_create(&child[0]) failed", error);
+            pthread_error("pthread_create() failed on", error, i);
         }
     }
 
     for (int i = 0; i < THREADS; i++) {
         error = pthread_join(child[i], NULL);
         if (error != SUCCESS) {
-            pthread_error("pthread_join() failed", error);
+            pthread_error("pthread_join() failed on", error, i);
         }
     }
 
